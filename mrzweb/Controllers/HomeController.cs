f5357f9f0b95581ec4e495eca471ccd714e9d518
wfsiew <siewwingfei@hotmail.com>
2016-06-07 21:24:49 +0800

    init

---
+++ mrzweb/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RestSharp;
+using Newtonsoft.Json;
+using Validator;
+
+namespace mrzweb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const bool DEBUG = true;
+
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Validate(MRZForm x)
+        {
+            Dictionary<string, object> res = new Dictionary<string, object>();
+
+            try
+            {
+                var client = new RestClient("http://localhost:5000");
+                var request = new RestRequest("api/mrz/validate", Method.POST);
+                request.RequestFormat = DataFormat.Json;
+                request.AddJsonBody(x);
+                var response = client.Execute<MRZ>(request);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    res["result"] = response.Data;
+                    res["success"] = 1;
+                }
+                    
+                else
+                {
+                    res["error"] = 1;
+                    res["message"] = response.ErrorMessage;
+                }
+            }
+
+            catch (Exception e)
+            {
+                res["error"] = 1;
+                res["message"] = e.StackTrace;
+            }
+
+            return Json(res, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ mrzweb/static/js/controller.js
@@ -0,0 +1,71 @@
+﻿function IndexCtrl($scope, $http) {
+
+    $scope.model = {
+        PassportNum: null,
+        Nationality: null,
+        DOB: {
+            year: null,
+            month: null,
+            day: null
+        },
+        Exp: {
+            year: null,
+            month: null,
+            day: null
+        },
+        Sex: null,
+        Line: null
+    };
+
+    $scope.result = {};
+
+    $scope.formsubmit = function () {
+        var x = $scope.model;
+        x.DOB.year = $scope.paddzero(x.DOB.year);
+        x.DOB.month = $scope.paddzero(x.DOB.month);
+        x.DOB.day = $scope.paddzero(x.DOB.day);
+
+        x.Exp.year = $scope.paddzero(x.Exp.year);
+        x.Exp.month = $scope.paddzero(x.Exp.month);
+        x.Exp.day = $scope.paddzero(x.Exp.day);
+
+        var o = {
+            PassportNum: x.PassportNum,
+            Nationality: x.Nationality,
+            DOB: x.DOB.year + x.DOB.month + x.DOB.day + '',
+            Sex: x.Sex,
+            Expiry: x.Exp.year + x.Exp.month + x.Exp.day + ''
+        };
+        $http.post("/Home/Validate", o).success(function (data) {
+            if (data.success == 1) {
+                $scope.result = data.result;
+                $scope.model.Line = data.result.Line;
+            }
+
+            else {
+                alert(data.message);
+            }
+        });
+    }
+
+    $scope.paddzero = function (a) {
+        if (!$scope.isint(a))
+            return '' + a;
+
+        var i = parseInt(a, 10);
+        var s = i < 10 ? '0' + i : '' + i;
+        return s;
+    }
+
+    $scope.isint = function (value) {
+        return !isNaN(value) &&
+               !isNaN(parseInt(value, 10));
+    }
+
+    $scope.status = function (a) {
+        var s = a == true ? 'pass' : 'fail';
+        return s;
+    }
+}
+
+app.controller('IndexCtrl', ['$scope', '$http', IndexCtrl]);
